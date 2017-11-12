931985eb4d72ac3b691d390ac969bd040dcd004c
Barkha Patel <barkha0292@gmail.com>
2017-11-11 18:47:56 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 var express = require("express");
 var path = require("path");
 var session = require('express-session');
-// create the express app
 var app = express();
-// var bodyParser = require('body-parser');
 
-app.use(session({secret: 'secretcode'})); 
 // use it!
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// static content
-// app.use(express.static(path.join(__dirname, "./static")));
-
-// setting up ejs and our views folder
+app.use(session({secret: 'secretcode'})); 
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
